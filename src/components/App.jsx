ed1c5a702d855e236234b554e17ae8a77db00dab
Carlo van Wyk <5310264+thecarlo@users.noreply.github.com>
2020-05-18 11:46:49 +1000

    Added github link; stylesheet changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import GeneratePassword from "./GeneratePassword";
-import "../assets/css/app.css";
+import "../assets/css/app.scss";
 
 class App extends Component {
+
   render() {
     return (
-      <div id="app-container" className="container">
-        <h1 id="heading">Strong password generator</h1>
-        <GeneratePassword />
-      </div>
+      <React.Fragment>
+        <div id="app-container" className="container">
+          <h1 id="heading">Strong password generator</h1>
+          <GeneratePassword />
+        </div>
+
+        <div id="footer-container" className="container">
+          <a target="_blank" title="Source code on Github" href="https://github.com/thecarlo/strongpw.com"><i id="github-icon" className="fab fa-github"></i> Source code</a>
+        </div>
+      </React.Fragment>
     );
   }
 }
