36cb3a57ffc4b9b5eae258b23649b98841c1afb2
Carlo van Wyk <5310264+thecarlo@users.noreply.github.com>
2024-04-27 08:36:31 +1000

    shufflePassword: use crypto.getRandomValues

--- src/functions/generate/shufflePassword.ts
+++ src/functions/generate/shufflePassword.ts
@@ -1,8 +1,12 @@
 export const shufflePassword = (password: string): string => {
   const array = password.split('');
 
+  const randomValues = new Uint32Array(array.length);
+
+  crypto.getRandomValues(randomValues);
+
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = randomValues[i] % (i + 1);
 
     [array[i], array[j]] = [array[j], array[i]];
   }
